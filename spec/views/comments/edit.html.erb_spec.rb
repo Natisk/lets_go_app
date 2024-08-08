df96daf721eea633cc87fba643275794ac028df7
Vitaly Matseiko <realnatisk@gmail.com>
2024-08-08 14:30:08 +0200

    add comment scaffold

---
+++ spec/views/comments/edit.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "comments/edit", type: :view do
+  let(:comment) {
+    Comment.create!(
+      post: nil,
+      user: nil,
+      body: "MyText"
+    )
+  }
+
+  before(:each) do
+    assign(:comment, comment)
+  end
+
+  it "renders the edit comment form" do
+    render
+
+    assert_select "form[action=?][method=?]", comment_path(comment), "post" do
+
+      assert_select "input[name=?]", "comment[post_id]"
+
+      assert_select "input[name=?]", "comment[user_id]"
+
+      assert_select "textarea[name=?]", "comment[body]"
+    end
+  end
+end
